4f7382f5a1688a6572087185efe68a913e325cb0
WM520 <1071597946@qq.com>
2016-05-25 15:55:23 +0800

    Initial Commit

---
+++ scrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollView
+//
+//  Created by David on 16/5/25.
+//  Copyright © 2016年 WM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
